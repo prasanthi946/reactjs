f0c50bfe1b6113f4a002cf783d77da88de7d4022
Prasanthi Revu <prasanthi.revu@otsi.com>
2023-12-08 12:34:30 +0530

    Initial commit

---
+++ data.js
@@ -0,0 +1,14 @@
+function x() {
+   for ( let i=1; i <= 5; i++){ 
+     setTimeout(function () {
+    console.log(i);
+      },i * 1000);
+
+    }  
+    console.log("JavaSrcipt");
+}
+x();
+function y() {
+  let p=20;
+  console.log(p)
+}
